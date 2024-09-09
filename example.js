c33454ceed0596d3bf2ac54d84c2387eec67b21c
musicslayer <musicslayer@gmail.com>
2024-09-09 13:21:10 -0400

    Update example

--- example.js
+++ example.js
@@ -22,10 +22,29 @@ const options = {
     },
 }
 
-// Send an email directly.
-// - This requires port 24 (LMTP) or 25 (SMTP) to be open, something that residential ISPs do not always allow.
-// - If an email is sent from a residential IP address, an email server may reject it before it even gets to the intended recipients.
-let results = (async () => await emailsend.sendEmail(mail, options))();
+async function init() {
+    // Send an email directly.
+    // - This requires port 24 (LMTP) or 25 (SMTP) to be open, something that residential ISPs do not always allow.
+    // - If an email is sent from a residential IP address, an email server may reject it before it even gets to the intended recipients.
+    let results = await emailsend.sendEmail(mail, options);
 
-// Send the email to a relay server, who will then send it to the intended recipients.
-let results2 = (async () => await emailsend.relayEmail(mail, 465, "smtp.gmail.com", options))();
+    if(Object.keys(results).length >= 0) {
+        console.log("sendEmail errors:");
+        for(let domain in results) {
+            let error = results[domain];
+            console.log(domain + "\n" + error.message + "\n" + error.stack);
+        }
+    }
+
+    // Send the email to a relay server, who will then send it to the intended recipients.
+    let results2 = await emailsend.relayEmail(mail, 465, "smtp.gmail.com", options);
+
+    if(Object.keys(results2).length >= 0) {
+        console.log("relayEmail errors:");
+        for(let domain in results2) {
+            let error = results2[domain];
+            console.log(domain + "\n" + error.message + "\n" + error.stack);
+        }
+    }
+}
+init();
